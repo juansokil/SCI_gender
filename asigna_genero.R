fb1dd6b504146cb01514b00db8850816e655d740
Juan Sokil <juanpablosokil@gmail.com>
2018-06-01 11:14:49 -0300

    calcula probabilidad

--- asigna_genero.R
+++ asigna_genero.R
@@ -1,4 +1,4 @@
-
+#install.packages("rPython")
 
 #####LIBRERIAS
 #install.packages("DBI")
@@ -10,17 +10,32 @@ library(Hmisc)
 ##install.packages("gender")
 library(gender)
 
+
+setwd("~/SCI_genero")
 source('./00-sql.r', encoding = 'latin1')
 
 ###chequear conexion###
+sci_ibero
 
 
-Author <- read_csv("~/SCI_genero/Author.csv")
+country <- 'Argentina'
+argentina_total <- dbSendQuery(sci_ibero, "select a.ut, a.order, a.name, a.surname, a.country from author_address a where country='Argentina'")
+argentina_total <-  fetch(argentina_total, n=-1)
 
 
-nombres <- unique(Author$Nombre)
 
-#gender(names, years = c(1932, 2012), method = c("ssa", "ipums", "napp","kantrowitz", "genderize", "demo"), countries = c("United States", "Canada","United Kingdom", "Denmark", "Iceland", "Norway", "Sweden"))
+###Extrae nombres y crea variables
+argentina_total$primer_nombre <- sapply(strsplit(argentina_total$surname, " "), "[", 1)
+argentina_total$segundo_nombre <- sapply(strsplit(argentina_total$surname, " "), "[", 2)
 
-Prob <- gender(nombres)
-View(Prob)
+######CALCULA PROBA#####
+nombres <- unique(argentina_total$primer_nombre)
+nombres_prob <- gender(nombres)
+nombres_prob <- nombres_prob[c(1,3,4)]
+
+
+####Pega la tabla al sql
+dbWriteTable(sci_ibero, name='nombres_prob', value=nombres_prob)
+dbListTables(sci_ibero)
+
+
