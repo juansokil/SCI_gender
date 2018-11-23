b06ad59d9264e030877253c365b822d5060ce126
Juan Pablo Sokil <juanpablosokil@gmail.com>
2018-11-23 16:16:46 -0300

    Add files via upload

---
+++ reg_ex.R
@@ -0,0 +1,62 @@
+library(DBI)
+library(RMySQL)
+library(Hmisc)
+library(stringr)
+
+
+scopus_ibero = dbConnect(MySQL(), user='scopus', password='sc0pus2698', dbname='scopus_ibero', host='mysql.ricyt.org')
+#article <- dbGetQuery(scopus_ibero, "select * from contact")
+
+##### Extracting a String Between 2 Characters in R
+##### By Eric Cai - The Chemical Statistician
+# getstr() is my customized function
+# it extracts a string between 2 characters in a string variable
+getstr = function(mystring, initial.character, final.character)
+{
+  
+  # check that all 3 inputs are character variables
+  if (!is.character(mystring))
+  {
+    stop('The parent string must be a character variable.')
+  }
+  
+  if (!is.character(initial.character))
+  {
+    stop('The initial character must be a character variable.')
+  }
+  
+  if (!is.character(final.character))
+  {
+    stop('The final character must be a character variable.')
+  }
+  
+  # pre-allocate a vector to store the extracted strings
+  snippet = rep(0, length(mystring))
+  
+  for (i in 1:length(mystring))
+  {
+    # extract the initial position
+    initial.position = gregexpr(initial.character, mystring[i])[[1]][1] + 1
+    # extract the final position
+    final.position = gregexpr(final.character, mystring[i])[[1]][1] - 1
+    # extract the substring between the initial and final positions, inclusively
+    snippet[i] = substr(mystring[i], initial.position, final.position)
+  }
+  return(snippet)
+}
+
+#
+
+
+
+
+
+
+
+article2 <- dbGetQuery(scopus_ibero, "select * from mails_edu a, article b where a.ut=b.ut")
+article2$mail <- as.data.frame(str_extract(article2$email, '\\S+$'))
+article2$autor <- substr(article2$email,1,regexpr(";",article2$email)-1)
+article2$country <- str_extract(getstr(article2$email, '*,', '; email:'),regex("\\S+$"))
+
+
+
